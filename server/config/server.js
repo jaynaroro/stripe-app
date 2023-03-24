afdefdc90d3914881d9ab458ea48ea1af664b12a
jaynaroro <jaynaroro@JaysWin32>
2023-03-24 16:56:47 +0300

    All done

--- server/config/server.js
+++ server/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'https://strapi-production-8617.up.railway.app/',
   app: {
     keys: env.array('APP_KEYS'),
   },
